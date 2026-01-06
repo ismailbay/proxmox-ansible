829e3522d4929be8e5cd2a42d1d10099b46ca704
Ismail Bay <ismailbay@gmail.com>
2026-01-06 09:29:33 +0100

    feat: handover provisioning to opentofu

---
+++ opentofu/providers.tf
@@ -0,0 +1,31 @@
+terraform {
+  required_version = ">= 1.6.0"
+
+  required_providers {
+    proxmox = {
+      source  = "bpg/proxmox"
+      version = "0.91"
+    }
+    sops = {
+      source  = "carlpett/sops"
+      version = "~> 1.3"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3"
+    }
+  }
+}
+
+data "sops_file" "secrets" {
+  source_file = "${path.module}/secrets.sops.yaml"
+}
+
+provider "proxmox" {
+  endpoint  = data.sops_file.secrets.data["proxmox.endpoint"]
+  api_token = data.sops_file.secrets.data["proxmox.api_token"]
+  ssh {
+    agent = false
+    private_key = file("~/.ssh/id_ansible_pve.pub") # TODO: create new keys
+  }
+}
